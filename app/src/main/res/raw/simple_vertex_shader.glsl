1f581083e82d7693211652d0676e632c42854fb8
徐乐宇 <xlyxly0502>
2022-08-08 14:13:30 +0800

    ""

--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -1,8 +1,7 @@
-uniform mat4 u_Matrix;
 attribute vec4 a_Position;
 
 void main() {
-    gl_Position = u_Matrix * a_Position;
+    gl_Position = a_Position;
     // 使用gl_PointSize 设置点的大小。
     //原理： 当OpenGlad把一个点分解为片段的时候，它会生成一些片段，
     //它们是以gl_PointSize为中心的四边形，这个四边形的每条边的长度与gl_PointSize相等。
